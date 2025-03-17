50a135d1f225f007fcdf2b77d351949087a5ea27
CodinJack <IIT2022005@iiita.ac.in>
2025-03-17 20:48:20 +0530

    added generation of questions through gemini

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quiz',  # Add your app here
+    'rest_framework',
+    'quiz',
 ]
+
 
 
 MIDDLEWARE = [
