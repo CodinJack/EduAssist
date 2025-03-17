654ea195dcc0c65636881a79149467727020766f
CodinJack <IIT2022005@iiita.ac.in>
2025-03-17 17:32:26 +0530

    added a new backend system

--- backend/quiz/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import get_questions
-
-urlpatterns = [
-    path('questions/', get_questions),
-]
--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -1,18 +1,3 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .models import Question
-from .serializers import QuestionSerializer
-import random
+from django.shortcuts import render
 
-@api_view(['GET'])
-def get_questions(request):
-    topic = request.GET.get('topic', None)  # Get topic from request
-    if topic:
-        questions = Question.objects.filter(tags__contains=topic)
-    else:
-        questions = Question.objects.all()
-    
-    questions = random.sample(list(questions), min(15, len(questions)))  # Get 15 random questions
-    serializer = QuestionSerializer(questions, many=True)
-    
-    return Response(serializer.data)
+# Create your views here.
