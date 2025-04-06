91644a046dc28eb853470fa78574d30fda5226d4
CodinJack <IIT2022005@iiita.ac.in>
2025-04-06 13:36:33 +0530

    added firebase credentials to env

--- backend/backend/firebase_config.py
+++ backend/backend/firebase_config.py
@@ -1,10 +1,22 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
 
-SERVICE_ACCOUNT_KEY_PATH = "firebase_config.json"
+firebase_credentials = {
+    "type": os.getenv("GOOGLE_TYPE"),
+    "project_id": os.getenv("GOOGLE_PROJECT_ID"),
+    "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("GOOGLE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
+    "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+    "auth_uri": os.getenv("GOOGLE_AUTH_URI"),
+    "token_uri": os.getenv("GOOGLE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("GOOGLE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("GOOGLE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("GOOGLE_UNIVERSE_DOMAIN"),
+}
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate(SERVICE_ACCOUNT_KEY_PATH)
-    # firebase_admin.initialize_app(cred)
-
+cred = credentials.Certificate(firebase_credentials)
+# firebase_admin.initialize_app(cred)
 db = firestore.client()
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,19 +16,27 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 from dotenv import load_dotenv
 load_dotenv()  
-
+import firebase_admin
 import os
-BASE_DIR1 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Points to EduAssist/backend
-FIREBASE_CRED_PATH = os.path.join(BASE_DIR1, "firebase_config.json")  # Points to the JSON file
 
+firebase_credentials = {
+    "type": os.getenv("GOOGLE_TYPE"),
+    "project_id": os.getenv("GOOGLE_PROJECT_ID"),
+    "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("GOOGLE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
+    "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+    "auth_uri": os.getenv("GOOGLE_AUTH_URI"),
+    "token_uri": os.getenv("GOOGLE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("GOOGLE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("GOOGLE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("GOOGLE_UNIVERSE_DOMAIN"),
+}
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate(os.getenv("FIREBASE_CONFIG_PATH"))
-    firebase_admin.initialize_app(cred)
+cred = credentials.Certificate(firebase_credentials)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-db = firestore.client() 
-
-print(BASE_DIR1)
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -67,7 +75,7 @@ INSTALLED_APPS = [
     'userAuth',
     'drf_yasg',
     'practice',
-    'tutorial',
+    'tutorials',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
